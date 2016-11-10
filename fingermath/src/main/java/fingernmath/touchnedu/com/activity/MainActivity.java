1cac581032682bdbd651f63001ee6e5d3da6acde
fingernmath <contact@touchnedu.com>
2016-11-10 09:28:20 +0900

    2016-11-10

--- fingermath/src/main/java/fingernmath/touchnedu/com/activity/MainActivity.java
+++ fingermath/src/main/java/fingernmath/touchnedu/com/activity/MainActivity.java
@@ -2,17 +2,26 @@ package fingernmath.touchnedu.com.activity;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import fingernmath.touchnedu.com.R;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.myscript.atk.math.widget.MathWidgetApi;
 import com.myscript.certificate.MyCertificate;
 
+import fingernmath.touchnedu.com.BuildConfig;
+import fingernmath.touchnedu.com.R;
 
-public class MainActivity extends Activity {
+
+public class MainActivity extends Activity implements MathWidgetApi.OnConfigureListener,
+                                                      MathWidgetApi.OnRecognitionListener {
+  private static final String TAG = "MathWidget";
+  private Context mContext;
   private MathWidgetApi mWidget;
 
   @Override
@@ -21,12 +30,41 @@ public class MainActivity extends Activity {
     requestWindowFeature(Window.FEATURE_NO_TITLE);
     setContentView(R.layout.activity_main);
 
-    mWidget = (MathWidgetApi) findViewById(R.id.mathWidget);
+    mContext = this;
+
     widgetInit();
+    viewInit();
+
 
   }
 
+  private void viewInit() {
+    Button menuBtn = (Button)findViewById(R.id.btn_menu_quiz),
+           soundBtn = (Button)findViewById(R.id.btn_sound);
+    menuBtn.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+//        Toast.makeText(mContext, "메뉴 버튼", Toast.LENGTH_SHORT).show();
+        mWidget.clear(true);
+      }
+    });
+    soundBtn.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        Toast.makeText(mContext, "소리 버튼", Toast.LENGTH_SHORT).show();
+      }
+    });
+  }
+
   private void widgetInit() {
+    Log.i(TAG, "widgetInit 진입");
+    mWidget = (MathWidgetApi)findViewById(R.id.mathWidget);
+    mWidget.setOnConfigureListener(this);
+    mWidget.setOnRecognitionListener(this);
+    mWidget.addSearchDir("zip://" + getPackageCodePath() + "!/assets/conf");
+    mWidget.configure("math", "standard");
+
+    Log.i(TAG, "Certificate : " + mWidget.registerCertificate(MyCertificate.getBytes()));
     if (!mWidget.registerCertificate(MyCertificate.getBytes())) {
       AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(this);
       dlgAlert.setMessage("Please use a valid certificate.");
@@ -57,5 +95,41 @@ public class MainActivity extends Activity {
     }
   }
 
+  @Override
+  public void onConfigureBegin(MathWidgetApi widget) {
 
+  }
+
+  @Override
+  public void onConfigureEnd(MathWidgetApi widget, boolean success) {
+    if(!success) {
+      Toast.makeText(getApplicationContext(), widget.getErrorString(), Toast.LENGTH_LONG).show();
+      Log.e(TAG, "Unable to configure the Math Widget : " + widget.getErrorString());
+      return;
+    }
+    Toast.makeText(getApplicationContext(), "Math Widget Configured", Toast.LENGTH_SHORT).show();
+    if (BuildConfig.DEBUG)
+      Log.d(TAG, "Math Widget configured!");
+  }
+
+  @Override
+  public void onRecognitionBegin(MathWidgetApi widget) {
+    Log.d(TAG, "Recognition Begun..");
+  }
+
+  @Override
+  public void onRecognitionEnd(MathWidgetApi widgetApi) {
+    Toast.makeText(getApplicationContext(), "Recognition update", Toast.LENGTH_SHORT).show();
+    if (BuildConfig.DEBUG) {
+      Log.d(TAG, "Math Widget recognition : " + widgetApi.getResultAsText());
+    }
+  }
+
+  @Override
+  protected void onDestroy() {
+    mWidget.setOnRecognitionListener(null);
+    mWidget.setOnConfigureListener(null);
+    mWidget.release();
+    super.onDestroy();
+  }
 }
